62b26c70de0dd84f1535404e3492b1152fe3ca0c
al5g20 <al5g20@soton.ac.uk>
2024-11-28 19:31:01 +0000

    Added channel setup base

--- ClocktowerBot.py
+++ ClocktowerBot.py
@@ -26,11 +26,17 @@ class Role(Enum): # Roles that are used to determine which channels players can 
     alive = "ctb-Alive" #Used to highlight players who are alive
     dead = "ctb-Dead" #Used to highlight dead players
     
+class ChannelNames(Enum): #Names for created discord channels and categories
+    category = "Blood on the clocktower"
+    storytellerText = "Story teller's lounge"
+    
+    
 class GameState: #Class the holds the users set for the game
     def __init__(self):
         self.storyteller = NULL #Meber who is the storyteller
         self.players = [] #List of players in the game
         self.active = False #Whever the game is running or not
+        self.channels = GameChannels() #Store game channels here
 
     def __str__(self) -> str:
         playerNames = []
@@ -51,6 +57,13 @@ class GameState: #Class the holds the users set for the game
             
     def getPlayers(self):
         return self.players
+    
+class GameChannels: #Holds the discord channels for use in the game
+    def __init__(self):
+        self.category = None #Category all the channels are grouped in
+        self.storytellerText = NULL #Storyteller text channel
+    
+    
         
 gameState = GameState() # public game state
         
@@ -95,14 +108,6 @@ async def setupRoles(interaction: discord.Interaction): # create roles used by t
         else:
             await interaction.guild.create_role(name=role.value, colour=discord.Colour(0x0062ff))
     await interaction.response.send_message("Initialised roles")
-
-@bot.tree.command(
-    name="setup_channels",
-    description="inits user channels for the bot",
-)
-async def setupChannels(interaction: discord.Interaction): # create the voice channels the bot will use
-     await interaction.guild.create_voice_channel(name="Town square")
-     await interaction.response.send_message("Initialised Channels")
      
 @bot.tree.command(
     name="set_story_teller",
@@ -173,6 +178,71 @@ async def removePlayer(interaction: discord.Interaction, member: discord.Member)
 )
 async def printGameState(interaction: discord.Interaction): #Print game state for testing TODO make this pretty
     await interaction.response.send_message(f"{gameState}")
-
+    
+@bot.tree.command(
+    name="sync_roles",
+    description="Syncs the bot to the bot-specific roles on the server and removes excess roles",
+)
+async def syncRoles(interaction: discord.Interaction): #Sync the discord roles to the bots game state, if possible
+    global gameState
+    if (gameState.active): #Changing the playlist mid-game will break things
+        await interaction.response.send_message("You cannot change the game's state while a match is active")
+    else:
+        try: #Roles might not exist or calling members may fail
+            memberList = interaction.guild.fetch_members() #Get all the servers members, might be dangerous but this bot has a limited scope in users
+            newGameState = GameState()
+            playerRole = get(interaction.guild.roles, name=Role.player.value) #Get player role from server
+            storyRole = get(interaction.guild.roles, name=Role.storyTeller.value) #Get storyteller role from server
+            async for member in memberList:
+                if (storyRole in member.roles) and (playerRole in member.roles): #A user is both storyteller and player
+                    await interaction.response.send_message(f"{member} cannot be both a player and a storyteller")
+                    return
+                if (storyRole in member.roles) and (newGameState.storyteller != NULL): #If a user is set as storyteller while another is a storyteller
+                    await interaction.response.send_message(f"{member} and {newGameState.storyteller} cannot be both be storytellers")
+                    return
+                if playerRole in member.roles:
+                    newGameState.addPlayer(member)
+                if storyRole in member.roles:
+                    newGameState.setStoryTeller(member)
+                    
+            gameState = newGameState #Update gamestate
+            await interaction.response.send_message(f"Synced member roles to the bot successfully")
+        except Exception as e:
+            print("Exception has occured while syncing player roles:",e)
+            await interaction.response.send_message("Something went wrong syncing roles")
+            
+async def createStoryText(interaction: discord.Interaction): #Create storyteller text channel
+    print(f"Category: {gameState.channels.category}")
+    storyRole = get(interaction.guild.roles, name=Role.storyTeller.value) #Get storyteller role from server
+    overwrites = {
+        interaction.guild.default_role: discord.PermissionOverwrite(read_messages=False),
+        storyRole: discord.PermissionOverwrite(read_messages=True)
+    }
+    gameState.channels.storytellerText = await interaction.guild.create_text_channel(name=ChannelNames.storytellerText.value, overwrites=overwrites, category=gameState.channels.category)
+    
+@bot.tree.command(
+    name="setup_channels",
+    description="creates the channels needed for the game if they do not exist",
+)
+async def setupChannels(interaction: discord.Interaction): #Creates the text and voice channels for the bot#
+    try:
+        if get(interaction.guild.categories,name=ChannelNames.category.value): # If category already exists
+            print(f"Category already exists")
+            gameState.channels.category = get(interaction.guild.categories,name=ChannelNames.category.value)
+        else:
+            category = await interaction.guild.create_category(ChannelNames.category.value)
+            gameState.channels.category = category
+            
+        await createStoryText(interaction)
+        await interaction.response.send_message("Succesfully created channels")
+        
+    except Exception as e:
+        print("Exception has occured while setting up channels:",e)
+        raise e
+        await interaction.response.send_message("Something went setting up channels")
+        
+    
+    
+            
 #Run the bot
 bot.run(TOKEN)
