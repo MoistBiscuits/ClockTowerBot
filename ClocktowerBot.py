1c2ad21a11cc89f498442299c007005a7936ba88
al5g20 <al5g20@soton.ac.uk>
2024-11-30 18:22:41 +0000

    Added bot channel creation

--- ClocktowerBot.py
+++ ClocktowerBot.py
@@ -1,6 +1,7 @@
 from asyncio.windows_events import NULL
 import os
 import random
+from tkinter.tix import INTEGER
 import discord
 import typing
 from enum import Enum
@@ -9,6 +10,7 @@ from discord import Interaction, app_commands
 from discord.utils import get
 from dotenv import load_dotenv
 from interactions import interaction
+from typing import List
 
 load_dotenv()
 TOKEN = os.getenv('DISCORD_TOKEN')
@@ -26,17 +28,30 @@ class Role(Enum): # Roles that are used to determine which channels players can 
     alive = "ctb-Alive" #Used to highlight players who are alive
     dead = "ctb-Dead" #Used to highlight dead players
     
-class ChannelNames(Enum): #Names for created discord channels and categories
+class ChannelNames(Enum): #Names for set created discord channels and categories
     category = "Blood on the clocktower"
-    storytellerText = "Story teller's lounge"
-    
-    
+    storytellerText = "Grimore"
+    storytellerVoice = "Story teller's lounge"
+    townText = "Town Record"
+    townVoice = "Town Square"
+    dayRooms=[
+        "Church",
+        "Bar",
+        "Courthouse",
+        "library",
+        "Docks",
+        "Market",
+        "Graveyard",
+        "Park"
+    ]
+
 class GameState: #Class the holds the users set for the game
     def __init__(self):
-        self.storyteller = NULL #Meber who is the storyteller
+        self.storyteller = None #Meber who is the storyteller
         self.players = [] #List of players in the game
         self.active = False #Whever the game is running or not
         self.channels = GameChannels() #Store game channels here
+        self.playerChannelDict = {} #Dictionary to stores which players have which private room
 
     def __str__(self) -> str:
         playerNames = []
@@ -58,10 +73,33 @@ class GameState: #Class the holds the users set for the game
     def getPlayers(self):
         return self.players
     
+    def addPrivateRoom(self,player: discord.Member,room: discord.channel):
+        if player in self.players:
+            self.playerChannelDict[player] = room
+            
+    def getRoomOfPlayer(self,player:discord.Member):
+        if player in self.players:
+            return self.playerChannelDict[player]
+        
+    
 class GameChannels: #Holds the discord channels for use in the game
     def __init__(self):
         self.category = None #Category all the channels are grouped in
-        self.storytellerText = NULL #Storyteller text channel
+        self.storytellerText = None #Storyteller text channel
+        self.storytellerVoice = None #Storyteller voice channel
+        self.townText = None #Town square AKA public day room where votes are called
+        self.townVoice = None #Town square AKA public day room where votes are called
+        self.publicRooms = [] #Public buildings used for daytime discussion, varies on player count/setttings
+        self.privateRooms = [] #Personal night rooms for each player, used for night abilities, length is player count
+        
+    def addPrivateRoom(self,room: discord.VoiceChannel):
+        if not (room in self.privateRooms):
+            self.privateRooms.append(room)
+            
+    def addPublicRoom(self,room: discord.VoiceChannel):
+        if not (room in self.publicRooms):
+            self.publicRooms.append(room)
+    
     
     
         
@@ -121,7 +159,7 @@ async def setStoryTeller(interaction: discord.Interaction, member: discord.Membe
         print(member)
         try: #Roles might not exist
             storyRole = get(interaction.guild.roles, name=Role.storyTeller.value) #Get storyteller role from server
-            if (gameState.storyteller != NULL): #If there was a previous storyteller, remove their role
+            if (gameState.storyteller != None): #If there was a previous storyteller, remove their role
                 await gameState.storyteller.remove_roles(storyRole)
             await member.add_roles(storyRole)
             gameState.setStoryTeller(member)
@@ -197,7 +235,7 @@ async def syncRoles(interaction: discord.Interaction): #Sync the discord roles t
                 if (storyRole in member.roles) and (playerRole in member.roles): #A user is both storyteller and player
                     await interaction.response.send_message(f"{member} cannot be both a player and a storyteller")
                     return
-                if (storyRole in member.roles) and (newGameState.storyteller != NULL): #If a user is set as storyteller while another is a storyteller
+                if (storyRole in member.roles) and (newGameState.storyteller != None): #If a user is set as storyteller while another is a storyteller
                     await interaction.response.send_message(f"{member} and {newGameState.storyteller} cannot be both be storytellers")
                     return
                 if playerRole in member.roles:
@@ -212,7 +250,6 @@ async def syncRoles(interaction: discord.Interaction): #Sync the discord roles t
             await interaction.response.send_message("Something went wrong syncing roles")
             
 async def createStoryText(interaction: discord.Interaction): #Create storyteller text channel
-    print(f"Category: {gameState.channels.category}")
     storyRole = get(interaction.guild.roles, name=Role.storyTeller.value) #Get storyteller role from server
     overwrites = {
         interaction.guild.default_role: discord.PermissionOverwrite(read_messages=False),
@@ -220,26 +257,111 @@ async def createStoryText(interaction: discord.Interaction): #Create storyteller
     }
     gameState.channels.storytellerText = await interaction.guild.create_text_channel(name=ChannelNames.storytellerText.value, overwrites=overwrites, category=gameState.channels.category)
     
+async def createStoryVoice(interaction: discord.Interaction): #Create storyteller voice channel
+    storyRole = get(interaction.guild.roles, name=Role.storyTeller.value) #Get storyteller role from server
+    overwrites = {
+        interaction.guild.default_role: discord.PermissionOverwrite(read_messages=False),
+        storyRole: discord.PermissionOverwrite(read_messages=True)
+    }
+    gameState.channels.storytellerVoice = await interaction.guild.create_voice_channel(name=ChannelNames.storytellerVoice.value, overwrites=overwrites, category=gameState.channels.category)
+    
+async def createTownText(interaction: discord.Interaction): #Create hub voice channel
+    dayRole = get(interaction.guild.roles, name=Role.day.value) #Get day role from server
+    storyRole = get(interaction.guild.roles, name=Role.storyTeller.value)
+    overwrites = {
+        interaction.guild.default_role: discord.PermissionOverwrite(read_messages=False),
+        dayRole: discord.PermissionOverwrite(read_messages=True),
+        storyRole: discord.PermissionOverwrite(read_messages=True)
+    }
+    gameState.channels.townText = await interaction.guild.create_text_channel(name=ChannelNames.townText.value, overwrites=overwrites, category=gameState.channels.category)
+    
+async def createTownVoice(interaction: discord.Interaction): #Create hub text channel
+    dayRole = get(interaction.guild.roles, name=Role.day.value) #Get day role from server
+    storyRole = get(interaction.guild.roles, name=Role.storyTeller.value)
+    overwrites = {
+        interaction.guild.default_role: discord.PermissionOverwrite(read_messages=False),
+        dayRole: discord.PermissionOverwrite(read_messages=True),
+        storyRole: discord.PermissionOverwrite(read_messages=True)
+    }
+    gameState.channels.townVoice = await interaction.guild.create_voice_channel(name=ChannelNames.townVoice.value, overwrites=overwrites, category=gameState.channels.category)
+
+def getInitRoomName(playerNumber: int): #Returns the name of a private room that should be used to create a players room
+    #Names used for each players private room
+    privateRoomNames=[
+        "Red Room",
+        "Blue Room",
+        "Yellow Room",
+        "Purple Room",
+        "Orange Room",
+        "Green Room",
+        "Cyan Room",
+        "Brown Room",
+        "Black Room",
+        "White Room",
+    ]
+    name = privateRoomNames[playerNumber % len(privateRoomNames)]
+    #TODO find a cleaner way to needing more room names that expected players
+    if playerNumber >= len(privateRoomNames): #If list overflows
+        name += " "
+        name += str(playerNumber // len(privateRoomNames)) #Append a unique number to it (e.g. Blue Room 2)
+    return name
+    
+async def createPrivateVoice(interaction: discord.Interaction, players: List[discord.Member]): #Create private rooms for each player
+    storyRole = get(interaction.guild.roles, name=Role.storyTeller.value)
+    for i in range(0,len(players)):
+        overwrites = {
+            interaction.guild.default_role: discord.PermissionOverwrite(read_messages=False),
+            players[i]: discord.PermissionOverwrite(read_messages=True),
+            storyRole: discord.PermissionOverwrite(read_messages=True)
+        }
+        room = await interaction.guild.create_voice_channel(name=getInitRoomName(i), overwrites=overwrites, category=gameState.channels.category)
+        gameState.channels.addPrivateRoom(room) # Add channel to channels
+        gameState.addPrivateRoom(players[i],room) # pair player to channel
+        
+async def createPublicVoice(interaction: discord.Interaction,count=8): #Creates the given amount of public rooms
+    storyRole = get(interaction.guild.roles, name=Role.storyTeller.value)
+    dayRole = get(interaction.guild.roles, name=Role.day.value)
+    for i in range(0,count):
+        overwrites = {
+            interaction.guild.default_role: discord.PermissionOverwrite(read_messages=False),
+            dayRole: discord.PermissionOverwrite(read_messages=True),
+            storyRole: discord.PermissionOverwrite(read_messages=True)
+        }
+        room = await interaction.guild.create_voice_channel(name=ChannelNames.dayRooms.value[i], overwrites=overwrites, category=gameState.channels.category)
+        gameState.channels.addPublicRoom(room)
+        
+ 
 @bot.tree.command(
     name="setup_channels",
     description="creates the channels needed for the game if they do not exist",
 )
 async def setupChannels(interaction: discord.Interaction): #Creates the text and voice channels for the bot#
+    if gameState.active:
+        await interaction.response.send_message(content=f"Cannot setup channels during an active game")
+        return
+    if gameState.getPlayers() == []:
+        await interaction.response.send_message(content=f"Cannot setup channels with no added players")
+        return
     try:
-        if get(interaction.guild.categories,name=ChannelNames.category.value): # If category already exists
-            print(f"Category already exists")
-            gameState.channels.category = get(interaction.guild.categories,name=ChannelNames.category.value)
-        else:
-            category = await interaction.guild.create_category(ChannelNames.category.value)
-            gameState.channels.category = category
-            
+        await interaction.response.defer(thinking=True, ephemeral=True)
+        category = get(interaction.guild.categories,name=ChannelNames.category.value)
+        if category: # If category already exists delete it
+            print(f"Category already exists, delete")
+            await category.delete()
+        gameState.channels.category = await interaction.guild.create_category(ChannelNames.category.value)
+
         await createStoryText(interaction)
-        await interaction.response.send_message("Succesfully created channels")
+        await createStoryVoice(interaction)
+        await createTownText(interaction)
+        await createTownVoice(interaction)
+        await createPublicVoice(interaction,8)
+        await createPrivateVoice(interaction,gameState.getPlayers())
+        await interaction.edit_original_response(content=f"Succesfully created channels")
         
     except Exception as e:
         print("Exception has occured while setting up channels:",e)
+        await interaction.edit_original_response(content=f"Something went setting up channels")
         raise e
-        await interaction.response.send_message("Something went setting up channels")
         
     
     
